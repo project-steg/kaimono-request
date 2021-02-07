e8a30aeacb5ded367bf16eb479386f9deca9bc37
Junya Oiwa <0920oiwa@gmail.com>
2021-02-07 18:46:14 +0900

    [add]GET /healthを作成

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
+const healthRouter = require("./routes/health")
+app.use("/health", healthRouter)
+
 /* サーバー起動 */
 app.listen(5000, () => {
   console.log("server started!")
