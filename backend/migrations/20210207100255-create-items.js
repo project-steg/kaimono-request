00253dfb36ed45399de1aee4e679b78276b290f2
Junya Oiwa <0920oiwa@gmail.com>
2021-02-07 19:15:36 +0900

    [add]itemsテーブルを定義

---
+++ backend/migrations/20210207100255-create-items.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      place: {
+        type: Sequelize.STRING
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('items');
+  }
+};
---
+++ backend/models/items.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class items extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  items.init({
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    place: DataTypes.STRING,
+    user_id: DataTypes.INTEGER,
+    created_at: DataTypes.TIMESTAMP,
+    updated_at: DataTypes.TIMESTAMP,
+  }, {
+    sequelize,
+    modelName: 'items',
+    underscored: true,
+  });
+  return items;
+};
